5390a602f8eb63ef888ecb4887fc5ea59461a456
Shrut <shrutpatel1120@gmail.com>
2025-01-11 14:31:47 -0600

    Add frontend as part of the main repository

---
+++ frontend/app/components/ContactButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactButton = () => {
+  return (
+    <div className="cursor-pointer hover:bg-airbnbDark transition mt-6 py-4 px-6 bg-airbnb text-white rounded-xl">
+        Contact
+    </div>
+  )
+}
+
+export default ContactButton
